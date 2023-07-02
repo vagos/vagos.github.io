a3bd2f5a407b1a52108fce5a48e3c6267e10f762
Evangelos Lamprou <e.lamprou@upnet.gr>
2023-07-02 17:38:25 +0300

    assets: updated profile picture js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
-profile_imgs = document.querySelectorAll(".profile");
-random_img = profile_imgs[Math.floor(Math.random() * profile_imgs.length)];
-random_img.style.display = "inline-block";
+let profile_imgs = document.querySelectorAll(".profile img");
+
+function getActiveProfile() {
+    for (let profile_img of profile_imgs) {
+        if (profile_img.style.display == "inline-block") {
+            return profile_img;
+        }
+    }
+    return null;
+}
+
+function setRandomPicture() {
+    let random_img = profile_imgs[Math.floor(Math.random() * profile_imgs.length)];
+    random_img.style.display = "inline-block";
+}
+
+setRandomPicture();
